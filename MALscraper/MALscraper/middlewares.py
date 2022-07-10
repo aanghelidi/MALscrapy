382af277c0a542a1226326d0dc211d6e39a786ce
aanghelidi <alainanghelidi@gmail.com>
2022-07-10 12:01:50 +0200

    [FIX] fix middleware

--- MALscraper/MALscraper/middlewares.py
+++ MALscraper/MALscraper/middlewares.py
@@ -1,12 +1,20 @@
+from __future__ import annotations
+
+from typing import Iterable
+
+# useful for handling different item types with a single interface
+# from itemadapter import ItemAdapter, is_item
+from scrapy import signals
+from scrapy.crawler import Crawler
+from scrapy.http.request import Request
+from scrapy.item import Item
+from scrapy.responsetypes import Response
+from scrapy.spiders import Spider
+
 # Define here the models for your spider middleware
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-
-from scrapy import signals
-
-# useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
 
 
 class MalscraperSpiderMiddleware:
@@ -15,45 +23,57 @@ class MalscraperSpiderMiddleware:
     # passed objects.
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler: Crawler) -> MalscraperSpiderMiddleware:
         # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
 
-    def process_spider_input(self, response, spider):
+    def process_spider_input(self, response: Response, spider: Spider) -> None:
         # Called for each response that goes through the spider
         # middleware and into the spider.
 
         # Should return None or raise an exception.
         return None
 
-    def process_spider_output(self, response, result, spider):
+    def process_spider_output(
+        self,
+        response: Response,
+        result: Iterable[Item | Request],
+        spider: Spider,
+    ) -> Iterable[Item | Request]:
         # Called with the results returned from the Spider, after
         # it has processed the response.
 
         # Must return an iterable of Request, or item objects.
-        for i in result:
-            yield i
+        yield from result
 
-    def process_spider_exception(self, response, exception, spider):
+    def process_spider_exception(
+        self,
+        response: Response,
+        exception: Exception,
+        spider: Spider,
+    ) -> None | Iterable[Item | Request]:
         # Called when a spider or process_spider_input() method
         # (from other spider middleware) raises an exception.
 
         # Should return either None or an iterable of Request or item objects.
         pass
 
-    def process_start_requests(self, start_requests, spider):
+    def process_start_requests(
+        self,
+        start_requests: Iterable[Request],
+        spider: Spider,
+    ) -> Iterable[Request]:
         # Called with the start requests of the spider, and works
         # similarly to the process_spider_output() method, except
         # that it doesn’t have a response associated.
 
         # Must return only requests (not items).
-        for r in start_requests:
-            yield r
+        yield from start_requests
 
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+    def spider_opened(self, spider: Spider) -> None:
+        spider.logger.info("Spider opened: %s" % spider.name)
 
 
 class MalscraperDownloaderMiddleware:
@@ -62,13 +82,17 @@ class MalscraperDownloaderMiddleware:
     # passed objects.
 
     @classmethod
-    def from_crawler(cls, crawler):
+    def from_crawler(cls, crawler: Crawler) -> MalscraperDownloaderMiddleware:
         # This method is used by Scrapy to create your spiders.
         s = cls()
         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
         return s
 
-    def process_request(self, request, spider):
+    def process_request(
+        self,
+        request: Request,
+        spider: Spider,
+    ) -> None | Response | Request:
         # Called for each request that goes through the downloader
         # middleware.
 
@@ -80,7 +104,12 @@ class MalscraperDownloaderMiddleware:
         #   installed downloader middleware will be called
         return None
 
-    def process_response(self, request, response, spider):
+    def process_response(
+        self,
+        request: Request,
+        response: Response,
+        spider: Spider,
+    ) -> Response | Request:
         # Called with the response returned from the downloader.
 
         # Must either;
@@ -89,7 +118,12 @@ class MalscraperDownloaderMiddleware:
         # - or raise IgnoreRequest
         return response
 
-    def process_exception(self, request, exception, spider):
+    def process_exception(
+        self,
+        request: Request,
+        exception: Exception,
+        spider: Spider,
+    ) -> None | Response | Request:
         # Called when a download handler or a process_request()
         # (from other downloader middleware) raises an exception.
 
@@ -99,5 +133,5 @@ class MalscraperDownloaderMiddleware:
         # - return a Request object: stops process_exception() chain
         pass
 
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+    def spider_opened(self, spider: Spider) -> None:
+        spider.logger.info("Spider opened: %s" % spider.name)
