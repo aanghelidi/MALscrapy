ad86cd65c476a9d60bfd61e2750105f53ebd6e46
aanghelidi <alainanghelidi@gmail.com>
2022-07-11 23:19:17 +0200

    [FEAT] add scrapy user-agent middleware

--- src/MALscraper/settings.py
+++ src/MALscraper/settings.py
@@ -50,9 +50,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'MALscraper.middlewares.MalscraperDownloaderMiddleware': 543,
-# }
+
+DOWNLOADER_MIDDLEWARES = {
+    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+    "scrapy_user_agents.middlewares.RandomUserAgentMiddleware": 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
