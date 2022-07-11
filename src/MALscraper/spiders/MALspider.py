ce579e680cbcad945c9e1cd2cc878517fae2f7f4
aanghelidi <alainanghelidi@gmail.com>
2022-07-11 09:25:57 +0200

    [FEAT] use AnimeLoader to scrape fields

--- src/MALscraper/spiders/MALspider.py
+++ src/MALscraper/spiders/MALspider.py
@@ -5,24 +5,22 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from ..items import AnimeItem
+from ..loaders import AnimeLoader
 
 
 class MALSpider(CrawlSpider):
+
     name: ClassVar[str] = "MALspider"
     allowed_domains: ClassVar[List[str]] = ["myanimelist.net"]
     start_urls: ClassVar[List[str]] = ["https://myanimelist.net/anime.php?letter=A"]
-
     rules = (Rule(LinkExtractor(allow=r"anime\/\d*"), callback="parse_anime"),)
 
     def parse_anime(self, response: Response) -> AnimeItem:
-        anime: AnimeItem = AnimeItem()
-        anime.title = response.xpath('//div[@itemprop="name"]/h1/strong/text()').get()
-        anime.jtitle = (
-            response.xpath(
-                'normalize-space(string(//span[@class="dark_text"]/text()[contains(.,"Japanese")]/parent::*/parent::*))',
-            )
-            .get()
-            .split(":")[-1]
-            .strip()
+        loader = AnimeLoader(item=AnimeItem(), response=response)
+        loader.add_xpath("title", '//div[@itemprop="name"]/h1/strong/text()')
+        loader.add_xpath(
+            "jtitle",
+            'string(//span[@class="dark_text"]/text()[contains(.,"Japanese")]/parent::*/parent::*)',
         )
-        return anime
+        anime_item: AnimeItem = loader.load_item()
+        return anime_item
